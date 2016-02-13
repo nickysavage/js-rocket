5465a1f223ede1754b5470cbfcd7ab97c0703dc8
Nick Savage <nick.savage@gmail.com>
2016-02-13 10:13:17 -0600

    dancing
    dancing

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+$(document).ready(function(e) {
+        var width = $(document).width();
+
+        function goRight() {
+            $("#animate").animate({
+            left: width
+          }, 5000, function() {
+             setTimeout(goLeft, 50);
+          });
+        }
+        function goLeft() {
+            $("#animate").animate({
+            left: 0
+          }, 5000, function() {
+             setTimeout(goRight, 50);
+          });
+        }
+
+        setTimeout(goRight, 50);
+    });
+
 // this function does this.
 var doCoolStuff = function () {
 	var currentClassName = document.getElementById('cool').className;
